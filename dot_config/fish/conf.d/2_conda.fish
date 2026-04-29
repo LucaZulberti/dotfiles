4222e6f28c1426d21dc0145b8731d9e9755c8556
Luca Zulberti <zulberti.luca@gmail.com>
2026-04-29 09:02:37 +0200

    fix(fish): replace conf.d file with generated from conda init

--- dot_config/fish/conf.d/2_conda.fish
+++ dot_config/fish/conf.d/2_conda.fish
@@ -4,9 +4,9 @@ if test -f $HOME/miniconda3/bin/conda
     eval $HOME/miniconda3/bin/conda "shell.fish" hook $argv | source
 else
     if test -f "$HOME/miniconda3/etc/fish/conf.d/conda.fish"
-        source "$HOME/miniconda3/etc/fish/conf.d/conda.fish"
+        . "$HOME/miniconda3/etc/fish/conf.d/conda.fish"
     else
-        set -gx PATH "$HOME/miniconda3/bin" $PATH
+        set -x PATH $HOME/miniconda3/bin $PATH
     end
 end
 # <<< conda initialize <<<
