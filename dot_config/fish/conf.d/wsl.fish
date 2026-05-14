1d1ffec563d220e7f5c16e87eb6cc26d4da314de
Luca Zulberti <zulberti.luca@gmail.com>
2026-05-14 10:05:04 +0200

    fix(fish): do not call git config in wsl.fish

--- dot_config/fish/conf.d/wsl.fish
+++ dot_config/fish/conf.d/wsl.fish
@@ -1,8 +1,3 @@
-if test -d /mnt/c/Users
-    alias ssh='ssh.exe'
-    alias ssh-add='ssh-add.exe'
-    alias scp='scp -S ssh.exe'
-
-    # Use windows ssh in Git
-    git config --global core.sshCommand ssh.exe
-end
+alias ssh='ssh.exe'
+alias ssh-add='ssh-add.exe'
+alias scp='scp -S ssh.exe'
