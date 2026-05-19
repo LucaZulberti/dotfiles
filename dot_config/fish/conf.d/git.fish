44649b84dbca59aca9c8bd9da29383dfa739dcd0
Luca Zulberti <zulberti.luca@gmail.com>
2026-05-19 20:27:06 +0200

    fix(fish): keep gl alias usable when devmoji is unavailable
    On a fresh machine where devmoji has not been installed yet, 'gl' failed with command not
    found. Fall back to plain 'git l --all --color' when devmoji is missing.

--- dot_config/fish/conf.d/git.fish
+++ dot_config/fish/conf.d/git.fish
@@ -1,9 +1,13 @@
 if type -q git
     alias gs="git status"
     alias gd="git diff"
-    alias gl="git l --all --color | devmoji --log --color"
     alias gp="git push"
     alias gP="git pull -p"
     alias gf="git fetch -p --all"
     alias gr="git rebase -i"
+    if type -q devmoji
+        alias gl="git l --all --color | devmoji --log --color"
+    else
+        alias gl="git l --all --color"
+    end
 end
