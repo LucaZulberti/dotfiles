65a1a5ec4af1ec3aea129d4a9223f662a4ca9382
Luca Zulberti <zulberti.luca@gmail.com>
2026-05-22 17:46:32 +0200

    feat(fish): improve zj function with new session

--- dot_config/fish/functions/zj.fish
+++ dot_config/fish/functions/zj.fish
@@ -1,15 +1,47 @@
 function zj
     set -l sessions (zellij list-sessions 2>/dev/null)
-    if test -n "$sessions"
-        set -l session (
-            printf '%s\n' $sessions | sk -1 --ansi | string replace -r '\[Created.*' '' | string trim
-        )
-        zellij attach "$session"
-    else
-        zellij
+    set -l new_entry "New session"
+
+    set -l selection (
+        begin
+            printf '%s\n' "$new_entry"
+            printf '%s\n' $sessions
+        end | sk -1 --ansi
+    )
+
+    if test -z "$selection"
+        return 1
     end
 
-    if test "$ZELLIJ_AUTO_EXIT" = true
-        kill $fish_pid
+    if test "$selection" = "$new_entry"
+        if set -q ZELLIJ
+            read -P "Session name: " session
+
+            if test -z "$session"
+                return 1
+            end
+
+            zellij action switch-session "$session"
+        else
+            zellij
+        end
+
+        return $status
+    end
+
+    set -l session (
+        printf '%s\n' "$selection" |
+        string replace -r '\s*\[Created.*$' '' |
+        string trim
+    )
+
+    if test -z "$session"
+        return 1
+    end
+
+    if set -q ZELLIJ
+        zellij action switch-session "$session"
+    else
+        zellij attach "$session"
     end
 end
