484472e9a4221e8bda9088a14ec460e14982872d
Luca Zulberti <zulberti.luca@gmail.com>
2026-05-26 14:20:32 +0200

    feat(fish/eza): use -g to display group in -l aliases

--- dot_config/fish/conf.d/eza.fish
+++ dot_config/fish/conf.d/eza.fish
@@ -2,15 +2,15 @@ if type -q eza
     alias ls="eza --icons"
     alias l="ls -1"
     alias la="ls -a"
-    alias ll="ls -la"
-    alias lr="ls -laR"
-    alias lt="ls -laT"
-    alias lm="ls -la -s modified -r"
+    alias ll="ls -lga"
+    alias lr="ls -lgaR"
+    alias lt="ls -lgaT"
+    alias lm="ls -lga -s modified -r"
     alias lf="ls -f"
-    alias lfl="ls -lf"
+    alias lfl="ls -lgf"
     alias ld="ls -D"
-    alias ldl="ls -lD"
-    alias lg="ls -la --git"
+    alias ldl="ls -lgD"
+    alias lg="ls -lga --git"
 
     function tree
         lt $extra_flags
