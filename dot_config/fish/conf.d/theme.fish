7be6711817b7a620dc040e0073db53bf2627ba03
Luca Zulberti <zulberti.luca@gmail.com>
2026-07-08 17:33:51 +0200

    fix(fish): correctly set the theme

--- dot_config/fish/conf.d/theme.fish
+++ dot_config/fish/conf.d/theme.fish
@@ -1,5 +1,4 @@
-if not set -q __fish_theme_saved
-    fish_config theme choose catppuccin-mocha
-    fish_config theme save
-    set -U __fish_theme_saved 1
+if status is-interactive
+    set -g fish_terminal_color_theme dark
+    fish_config theme choose catppuccin-mocha >/dev/null
 end
