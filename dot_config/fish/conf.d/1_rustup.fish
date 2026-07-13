6f7a73274d08c746c6e820d95bf1660bf4b976ee
Luca Zulberti <zulberti.luca@gmail.com>
2026-07-13 08:17:29 +0200

    feat(fish): warns about new Rust toolchain version available

--- dot_config/fish/conf.d/1_rustup.fish
+++ dot_config/fish/conf.d/1_rustup.fish
@@ -1,3 +1,26 @@
 if test -d $HOME/.cargo
     source "$HOME/.cargo/env.fish"
+
+    if status is-interactive
+        set -l stamp ~/.cache/rustup-last-check
+        mkdir -p ~/.cache
+
+        set -l today (date +%F)
+
+        if not test -f $stamp; or test (cat $stamp) != $today
+            echo $today >$stamp
+
+            set -l output (rustup check 2>/dev/null)
+
+            if string match -qr "Update available" -- $output
+                echo
+                set_color yellow
+                echo "⚠ Rust toolchain update available"
+                set_color normal
+                echo $output
+                echo
+                echo "Run: rustup update"
+            end
+        end
+    end
 end
